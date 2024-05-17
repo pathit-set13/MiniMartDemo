8c4639015c464e7eb5d3e43623b6d2cfc1aad2d5
Pathit <pathitta.se@gmail.com>
2024-05-17 21:36:39 +0700

    start project & create Employee package

---
+++ src/main/java/com/example/StoreSystemDemo/StoreSystemDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.StoreSystemDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreSystemDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreSystemDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StoreSystemDemo/StoreSystemDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StoreSystemDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreSystemDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
