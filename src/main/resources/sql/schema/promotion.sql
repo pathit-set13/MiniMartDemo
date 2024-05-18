73e12159a138fe39ee3ecb3e2e9ac83c32f996da
Pathit <pathitta.se@gmail.com>
2024-05-18 21:08:22 +0700

    crate promotion.sql

--- src/main/resources/sql/schema/promotion.sql
+++ src/main/resources/sql/schema/promotion.sql
@@ -1,2 +1,12 @@
 CREATE TABLE IF NOT EXISTS promotion (
+    promotion_id SERIAL PRIMARY KEY,
+    promoCode VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR NOT NULL,
+    description TEXT,
+    start_date TIMESTAMP NOT NULL,
+    end_date TIMESTAMP NOT NULL,
+    discount_amount DECIMAL(10,2)
+    product_skus VARCHAR(255) NOT NULL DEFAULT '',
+    min_quantity INT,
+    free_quantity INT
 );
