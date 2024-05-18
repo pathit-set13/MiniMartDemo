b797d8642121fbc95eb8ab45125e80d120a0ef14
Pathit <pathitta.se@gmail.com>
2024-05-18 20:30:36 +0700

    create schema of storage

--- src/main/resources/sql/schema/storage.sql
+++ src/main/resources/sql/schema/storage.sql
@@ -1,3 +1,8 @@
 CREATE TABLE storage (
-    barcode
+    itemId SERIAL PRIMARY KEY,
+    barcode VARCHAR(255) UNIQUE NOT NULL,
+    product_name VARCHAR(255) UNIQUE NOT NULL,
+    quantity INTEGER NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    description TEXT
 );
