7e5fb251b64ac5cfda7b2cef6c4b6226429ca0f7
Pathit <pathitta.se@gmail.com>
2024-05-18 21:09:46 +0700

    modify storage coluum name

--- src/main/resources/sql/schema/storage.sql
+++ src/main/resources/sql/schema/storage.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS storage (
-    itemId SERIAL PRIMARY KEY,
-    barcode VARCHAR(255) UNIQUE NOT NULL,
+    product_id SERIAL PRIMARY KEY,
+    product_barcode VARCHAR(255) UNIQUE NOT NULL,
     product_name VARCHAR(255) UNIQUE NOT NULL,
     quantity INTEGER NOT NULL,
     price DECIMAL(10,2) NOT NULL,
