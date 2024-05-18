5bffbca2844d57baecd2a6a7f6db3983dcdcb872
Pathit <pathitta.se@gmail.com>
2024-05-18 19:26:25 +0700

    rename 'Id' -> 'employeeId'

--- src/main/resources/sql/schema/employee_accounts.sql
+++ src/main/resources/sql/schema/employee_accounts.sql
@@ -1,5 +1,5 @@
 CREATE TABLE employee_account (
-    Id SERIAL PRIMARY KEY,
+    employeeId SERIAL PRIMARY KEY,
     username VARCHAR(30) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(255) NOT NULL
