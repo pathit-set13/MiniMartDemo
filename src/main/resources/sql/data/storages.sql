f030c2dfca90100f28cbd72527fc0fa9a3e17fe6
Pathit <pathitta.se@gmail.com>
2024-05-22 21:35:55 +0700

    modify storage schema & create storage data file

---
+++ src/main/resources/sql/data/storages.sql
@@ -0,0 +1,27 @@
+--Dairy Products
+INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
+    VALUES ('006594989977', 'FM Chocolate Milk 200ml.', 87.00, 40, 'packs', '6 pcs/pack');
+INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
+    VALUES ('006594989977', 'FM Chocolate Milk 200ml.', 15.00, 54, 'pcs', 'not included /w packs unit');
+INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
+    VALUES ('006594989970', 'FM Milk 200ml.', 87.00, 30, 'packs', '6 pcs/pack');
+INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
+    VALUES ('006594989970', 'FM Milk 200ml.', 15.00, 60, 'pcs', 'not included /w packs unit');
+INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
+    VALUES ('121231212100', 'Dutchman Yogurt 150ml.', 75.00, 34, 'packs', '4 pcs/pack');
+INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
+    VALUES ('121231212150', 'Dutchman Yogurt (Honey) 150ml.', 75.00, 27, 'packs', '4 pcs/pack');
+INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
+    VALUES ('789546136400', 'Linga Butter (Unsalted) 100g.', 49.00, 50, 'pcs', '-');
+INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
+    VALUES ('789546136400', 'Linga Butter (Salted) 100g.', 49.00, 48, 'pcs', '-');
+
+--Beverages
+INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
+    VALUES ('999745612385', 'KT Drinking Water 600ml.', 10.00, 100, 'pcs', 'not included /w packs unit');
+INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
+    VALUES ('999745612385', 'KT Drinking Water 600ml.', 58.00, 48, 'packs', '6 pcs/pack');
+INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
+    VALUES ('999745612479', 'KT Drinking Water 1.5L.', 15.00, 87, 'pcs', 'not included /w packs unit');
+INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
+    VALUES ('999745612479', 'KT Drinking Water 1.5L.', 89.00, 55, 'packs', '6 pcs/pack');
--- src/main/resources/sql/schema/storage.sql
+++ src/main/resources/sql/schema/storage.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS storage (
     product_id SERIAL PRIMARY KEY,
-    barcode VARCHAR(255) UNIQUE NOT NULL,
+    barcode VARCHAR(255) NOT NULL,
     product_name VARCHAR(255) UNIQUE NOT NULL,
-    quantity INTEGER NOT NULL,
     price DECIMAL(10,2) NOT NULL,
+    quantity INTEGER NOT NULL,
+    unit VARCHAR(255) NOT NULL DEFAULT 'unit',
     description TEXT
 );
