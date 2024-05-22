a801fd8bd30806ec28876fefa3bc871ebefa2e19
Pathit <pathitta.se@gmail.com>
2024-05-22 22:02:43 +0700

    update products data

--- src/main/resources/sql/data/storages.sql
+++ src/main/resources/sql/data/storages.sql
@@ -1,10 +1,10 @@
 --Dairy Products
 INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
-    VALUES ('006594989977', 'FM Chocolate Milk 200ml.', 87.00, 40, 'packs', '6 pcs/pack');
+    VALUES ('006594989977', 'FM Chocolate Milk 6x200ml.', 87.00, 40, 'packs', '6 pcs/pack');
 INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
     VALUES ('006594989977', 'FM Chocolate Milk 200ml.', 15.00, 54, 'pcs', 'not included /w packs unit');
 INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
-    VALUES ('006594989970', 'FM Milk 200ml.', 87.00, 30, 'packs', '6 pcs/pack');
+    VALUES ('006594989970', 'FM Milk 6x200ml.', 87.00, 30, 'packs', '6 pcs/pack');
 INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
     VALUES ('006594989970', 'FM Milk 200ml.', 15.00, 60, 'pcs', 'not included /w packs unit');
 INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
@@ -20,8 +20,16 @@ INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
 INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
     VALUES ('999745612385', 'KT Drinking Water 600ml.', 10.00, 100, 'pcs', 'not included /w packs unit');
 INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
-    VALUES ('999745612385', 'KT Drinking Water 600ml.', 58.00, 48, 'packs', '6 pcs/pack');
+    VALUES ('999745612385', 'KT Drinking Water 6x600ml.', 58.00, 48, 'packs', '6 pcs/pack');
 INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
     VALUES ('999745612479', 'KT Drinking Water 1.5L.', 15.00, 87, 'pcs', 'not included /w packs unit');
 INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
-    VALUES ('999745612479', 'KT Drinking Water 1.5L.', 89.00, 55, 'packs', '6 pcs/pack');
+    VALUES ('999745612479', 'KT Drinking Water 6x1.5L.', 89.00, 55, 'packs', '6 pcs/pack');
+INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
+    VALUES ('894156110548', 'SUZA Cola 350ml.', 15.00, 78, 'cans', '-');
+INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
+    VALUES ('894156114458', 'SUZA Cola 1.0L.', 25.00, 64, 'bottles', '-');
+INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
+    VALUES ('004849654670', 'LION SODA 500ml.', 15.00, 99, 'bottles', '-');
+INSERT INTO storage (barcode, product_name, price, quantity, unit, description)
+    VALUES ('004849654670', 'LION SODA 6x500ml.', 84.00, 99, 'packs', '6 pcs/pack');
