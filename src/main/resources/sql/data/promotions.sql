fb7b7e1e9c94f7f92a8785e0d9cb1fddff5f21f4
Pathit <pathitta.se@gmail.com>
2024-05-25 22:06:03 +0700

    add promotion data

--- src/main/resources/sql/data/promotions.sql
+++ src/main/resources/sql/data/promotions.sql
@@ -1,2 +1,4 @@
-INSERT INTO promotion (promoCode, name, description, start_date, end_date, discount_amount, min_quantity, free_quantity)
-VALUES ('HOTSUMMERTWENTY', 'Discount 20 THB. for Beverages', 'Discount 20 THB. for Beverage total price.', '2024-04-01', '2024-06-30', 20.00, 1)ON CONFLICT (code) DO NOTHING;
+INSERT INTO promotion (promoCode, name, description, start_date, end_date, product_barcode, discount_amount, min_spend, min_quantity, free_quantity)
+VALUES ('HOTSUMMER20', 'Discount 20 THB. for Beverages', 'Discount 20 THB. for Beverage total price.', '2024-04-01', '2024-06-30', '999745612385,999745612479,894156110548,894156114458,004849654670', 20.00, 0.00, 1, 0)ON CONFLICT (promoCode) DO NOTHING;
+INSERT INTO promotion (promoCode, name, description, start_date, end_date, product_barcode, discount_amount, min_spend, min_quantity, free_quantity)
+VALUES ('HOTSUMMER50', 'Discount 50 THB. for Beverages', 'Discount 50 THB. for Beverage total price at min spend 150 THB.', '2024-04-01', '2024-06-30', '999745612385,999745612479,894156110548,894156114458,004849654670', 50.00, 150.00, 1, 0)ON CONFLICT (promoCode) DO NOTHING;
